390ec5a44cafd5b0aa4f3f1f818a24d6290ea3d5
Tatsu ryu <15129457+tatsuryu@users.noreply.github.com>
2022-09-19 07:28:06 -0300

    feat: adding pwa

--- pwa/entrypoint.sh
+++ pwa/entrypoint.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+create_config() {
+    extrasjs=$(</home/app/config_js_extras)
+    modulejs=$(</home/app/config_js_exports)
+    echo "${extrasjs}" > shopware-pwa.config.js
+    echo "${modulejs/SHOPWARE_ACCESSTOKEN/${SHOPWARE_ACCESSTOKEN}}" >> \
+        shopware-pwa.config.js
+}
+
 start(){
     cd ${APP_PATH};
     yarn
@@ -13,9 +21,10 @@ start(){
 main (){
     APP_PATH=${APP_PATH:-"/home/app/pwa"}
     # SHOPWARE_ENDPOINT=${SHOPWARE_ENDPOINT?"Necessário informar o ENDPOINT"}
-    # SHOPWARE_ACCESSTOKEN=${SHOPWARE_ACCESSTOKEN?"Necessário informar o ACCESSTOKEN"}
+    SHOPWARE_ACCESSTOKEN=${SHOPWARE_ACCESSTOKEN?"Necessário informar o ACCESSTOKEN"}
+    cd ${APP_PATH}
 
-    # create_config;
+    create_config;
     start;
 }
 
